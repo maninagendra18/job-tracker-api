f19009ee4ecda7b6d13c31220caa717491272184
Jonnada Gangadhara Mani Nagendra <jgmnagendra1234@gmail.com>
2026-05-23 01:32:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///jobs.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = "jwt-secret"
-
-    # 🔥 Optional but good for interview
     JWT_ACCESS_TOKEN_EXPIRES = 3600  # 1 hour
